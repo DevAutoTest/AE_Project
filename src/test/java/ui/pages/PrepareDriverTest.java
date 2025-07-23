e81e7c222eab3f20a707e66ca3fb29feaaa8d555
tumba7788 <Elena_Danilova7788@outlook.com>
2025-07-23 17:57:08 +0200

    set driver

--- src/test/java/ui/pages/PrepareDriverTest.java
+++ src/test/java/ui/pages/PrepareDriverTest.java
@@ -17,7 +17,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Map;
-import java.util.Objects;
 
 @Getter
 @Feature("preparing driver")
